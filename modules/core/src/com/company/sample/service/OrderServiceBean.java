797ef9c79c6feedc3be73e9bbfae7543faee7fe2
Andrey Belyaev <belyaev.andrey@gmail.com>
2018-08-15 18:42:16 +0300

    Simplified code a bit

--- modules/core/src/com/company/sample/service/OrderServiceBean.java
+++ modules/core/src/com/company/sample/service/OrderServiceBean.java
@@ -3,11 +3,11 @@ package com.company.sample.service;
 import com.company.sample.core.repositories.OrderRepository;
 import com.company.sample.entity.Customer;
 import com.company.sample.entity.SalesOrder;
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service(OrderService.NAME)
@@ -19,9 +19,6 @@ public class OrderServiceBean implements OrderService {
     @Override
     @Transactional
     public List<SalesOrder> getSalesOrdersForCustomer(Customer customer) {
-        Iterable<SalesOrder> orderIterable = orderRepository.findAll();
-        List<SalesOrder> orders = new ArrayList<>();
-        orderIterable.forEach(orders::add);
-        return orders;
+        return Lists.newArrayList(orderRepository.findByCustomer(customer));
     }
 }
