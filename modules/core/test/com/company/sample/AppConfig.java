3dacbebdbedd106952e5e5529b18885e619698ee
Andrey Belyaev <belyaev.andrey@gmail.com>
2018-10-02 19:57:23 +0300

    Full project restructure

--- modules/core/test/com/company/sample/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.company.sample;
-
-//import EnableCubaRepositories;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@EnableCubaRepositories
-public class AppConfig {
-}
